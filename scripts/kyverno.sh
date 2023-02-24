7ee2bc2f094a410d13c0621a1997c919249e6b5a
JosephYostos <86612715+JosephYostos@users.noreply.github.com>
2023-02-24 00:24:38 +0000

    Update kyverno.sh

--- scripts/kyverno.sh
+++ scripts/kyverno.sh
@@ -8,6 +8,11 @@ helm repo add kyverno https://kyverno.github.io/kyverno/
 helm repo update
 # Install the Kyverno Helm chart into a new namespace called "kyverno"
 helm install kyverno kyverno/kyverno -n kyverno --create-namespace
-# create policy 
+# create namespace "enforce-namespace"
+kubectl create ns enforce-namespace
+#create secret from the public key 
+kubectl create secret generic cosign -n kyverno --from-file=cosign.pub
+#create policy 
+kubectl apply -f manifest/kyverno-policy.yaml
 
 
